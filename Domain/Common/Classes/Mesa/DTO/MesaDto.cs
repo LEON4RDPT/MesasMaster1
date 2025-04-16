ad84050f6b3fce4455693478bd8cab710ec429e1
leoo <avramadriana3@gmail.com>
2025-04-16 11:36:05 +0100

    Reserva GetAll + Reserva Database.

---
+++ Domain/Common/Classes/Mesa/DTO/MesaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Common.Classes.Mesa.DTO;
+
+public class MesaDto
+{
+    public int Id { get; set; }
+    public required int TimeLimit { get; set; } //minutes
+    public required int LocalX { get; set; }
+    public required int LocalY { get; set; }
+    public required int CapUsers { get; set; }
+}
--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -7,4 +7,5 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Mesa> Mesas { get; set; }
+    public DbSet<Reserva> Reservas { get; set; }
 }
